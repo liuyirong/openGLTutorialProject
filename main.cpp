bd82bc7ee3acd40cfd9ae7243fa4180fd1a7f932
liuyirong <1624739559@qq.com>
2016-12-13 01:07:18 +0800

    着色器
    为程序添加着色器，这样三角形就有了颜色信息，给所有顶点添加颜色信息

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include"GL\glew.h"
 #include"Display.h"
 #include "Mesh.h"
+#include "Shader.h"
 
 int main(int argc, char** argv)		//SDL2对main函数有特殊要求
 {
@@ -14,9 +15,12 @@ int main(int argc, char** argv)		//SDL2对main函数有特殊要求
 		Vertex(glm::vec3(0.5,-0.5,0)) };		//顶点数组对象
 	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));	//mesh对象，参数为顶点对象和顶点对象个数
 
+	Shader shader("./res/basicShader");  //构造shader对象
+
 	while (!display.IsClosed())		//如果程序没有退出,while大循环
 	{
 		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+		shader.Bind();	//使用着色器
 
 		mesh.Draw();
 
